fcfe3cc6e23d1b49410cace77fbc507dea612354
aguyran <aguyran@gmail.com>
2025-04-22 23:58:35 +0530

    feat: add regex to pass new line case

--- src/add.py
+++ src/add.py
@@ -1,5 +1,6 @@
+import re
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
-    numbers_list = numbers.split(",")
+    numbers_list = re.split(r"\\n|,|\n",numbers )
     return sum(int(number) for number in numbers_list)
